2863e8130e189669d3b0c868bd614e4a0f106013
Seitaro Kaneko <seitaro.kaneko@uec.ac.jp>
2017-03-16 14:12:00 -0400

    キャスト修正,Cmake設定修正

--- latero-graphics/graphics/groupwidget.cpp
+++ latero-graphics/graphics/groupwidget.cpp
@@ -74,7 +74,7 @@ GroupTreeView::GroupTreeView(GroupPtr peer) :
 void GroupTreeView::RebuildMenu(PatternPtr pattern)
 {
 	GroupPtr group = boost::dynamic_pointer_cast<Group>(pattern);
-	bool has_parent = GetParentGroup(pattern);
+	bool has_parent = (bool)etParentGroup(pattern);
 	TexturePtr tx = boost::dynamic_pointer_cast<Texture>(pattern);
 	
 	menu_.items().erase(menu_.items().begin(),menu_.items().end());
@@ -648,7 +648,7 @@ void GroupWidget::OnSelectionChanged()
 		TexturePtr tx = boost::dynamic_pointer_cast<Texture>(pattern);
 		ComboTexturePtr combotx = boost::dynamic_pointer_cast<ComboTexture>(pattern);
 		DotsPtr dots = boost::dynamic_pointer_cast<Dots>(pattern);
-		bool hasParent = (treeView_.GetParentGroup(pattern));
+		bool hasParent = (bool)(treeView_.GetParentGroup(pattern));
 
 		if (group)
 		{
