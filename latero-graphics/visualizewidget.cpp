b454f24070cf0e44e6c94b28021620531b774dc7
Vincent Levesque <vincent@vlevesque.com>
2026-03-13 17:59:47 +0900

    Re-enabled right-click on preview.

--- latero-graphics/visualizewidget.cpp
+++ latero-graphics/visualizewidget.cpp
@@ -77,38 +77,38 @@ VisualizeWidget::VisualizeWidget(PositionGenPtr gen) :
 
 Gtk::Widget *VisualizeWidget::GetAnimWidget()
 {
-	Gtk::Table *table = manage(new Gtk::Table(6,3));
+	auto table = manage(new Gtk::Grid());
 
-	table->attach(*manage(new Gtk::Label("Frames:", Gtk::ALIGN_START)), 	0,1, 0,1, Gtk::FILL);
-	table->attach(*manage(new Gtk::Label("Interval:", Gtk::ALIGN_START)), 	0,1, 1,2, Gtk::FILL);
-	table->attach(*manage(new Gtk::Label("Start:", Gtk::ALIGN_START)), 	0,1, 2,3, Gtk::FILL);
-	table->set_col_spacing(0,12);
+	table->attach(*manage(new Gtk::Label("Frames:", Gtk::ALIGN_START)),0,0,1,1); // 0,1, 0,1, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label("Interval:   ", Gtk::ALIGN_START)),0,1,1,1); // 0,1, 1,2, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label("Start:", Gtk::ALIGN_START)),0,2,1,1); //0,1, 2,3, Gtk::FILL);
+	//table->set_col_spacing(0,12);
 
-	table->attach(*manage(new Gtk::SpinButton(nbFramesAdj_)),		1,2, 0,1);
-	table->attach(intervalCtrl_,				 		1,2, 1,2);
-	table->attach(startTimeCtrl_,						1,2, 2,3);
-	table->set_col_spacing(1,30);
+	table->attach(*manage(new Gtk::SpinButton(nbFramesAdj_)),1,0,1,1); // 1,2, 0,1);
+	table->attach(intervalCtrl_,1,1,1,1); // 1,2, 1,2);
+	table->attach(startTimeCtrl_,1,2,1,1); // 1,2, 2,3);
+	//table->set_col_spacing(1,30);
 
-	table->attach(*manage(new Gtk::Label("Mode:", Gtk::ALIGN_START)), 	2,3, 0,1, Gtk::FILL);
-	table->attach(*manage(new Gtk::Label("Width:", Gtk::ALIGN_START)), 	2,3, 1,2, Gtk::FILL);
-	table->attach(*manage(new Gtk::Label("Velocity:", Gtk::ALIGN_START)), 	2,3, 2,3, Gtk::FILL);
-	table->set_col_spacing(2,12);
+	table->attach(*manage(new Gtk::Label("      Mode:", Gtk::ALIGN_START)),2,0,1,1); // 2,3, 0,1, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label("      Width:", Gtk::ALIGN_START)),2,1,1,1); // 2,3, 1,2, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label("      Velocity:   ", Gtk::ALIGN_START)),2,2,1,1); // 2,3, 2,3, Gtk::FILL);
+	//table->set_col_spacing(2,12);
 
-	table->attach(modeCombo_,				 		3,7, 0,1);
-	table->attach(*manage(new Gtk::SpinButton(widthAdj_)),			3,4, 1,2);
-	table->attach(*manage(new Gtk::SpinButton(velMagAdj_)), 		3,4, 2,3);
-	table->set_col_spacing(3,6);
+	table->attach(modeCombo_,3,0,4,1); // 3,7, 0,1);
+	table->attach(*manage(new Gtk::SpinButton(widthAdj_)),3,1,1,1); // 3,4, 1,2);
+	table->attach(*manage(new Gtk::SpinButton(velMagAdj_)),3,2,1,1); // 3,4, 2,3);
+	//table->set_col_spacing(3,6);
 
-	table->attach(*manage(new Gtk::Label("x")), 				4,5, 1,2, Gtk::FILL);
-	table->attach(*manage(new Gtk::Label("mm/s", Gtk::ALIGN_START)), 	4,5, 2,3, Gtk::FILL);
-	table->set_col_spacing(4,12);
+	table->attach(*manage(new Gtk::Label("x")),4,1,1,1); // 4,5, 1,2, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label(" mm/s ", Gtk::ALIGN_START)),4,2,1,1); // 4,5, 2,3, Gtk::FILL);
+	//table->set_col_spacing(4,12);
 
-	table->attach(*manage(new Gtk::SpinButton(heightAdj_)),			5,6, 1,2);
-	table->attach(*manage(new Gtk::SpinButton(velDirAdj_)), 		5,6, 2,3);
-	table->set_col_spacing(5,6);
+	table->attach(*manage(new Gtk::SpinButton(heightAdj_)),5,1,1,1); // 5,6, 1,2);
+	table->attach(*manage(new Gtk::SpinButton(velDirAdj_)),5,2,1,1); // 5,6, 2,3);
+	//table->set_col_spacing(5,6);
 
-	table->attach(*manage(new Gtk::Label("pixels", Gtk::ALIGN_START)), 	6,7, 1,2, Gtk::FILL);
-	table->attach(*manage(new Gtk::Label("degrees", Gtk::ALIGN_START)), 	6,7, 2,3, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label(" pixels", Gtk::ALIGN_START)),6,1,1,1); // 6,7, 1,2, Gtk::FILL);
+	table->attach(*manage(new Gtk::Label(" degrees", Gtk::ALIGN_START)),6,2,1,1); // 6,7, 2,3, Gtk::FILL);
 
 	widthAdj_->signal_value_changed().connect(sigc::mem_fun(*this, &VisualizeWidget::OnWidthChanged));
 	heightAdj_->signal_value_changed().connect(sigc::mem_fun(*this, &VisualizeWidget::OnHeightChanged));
